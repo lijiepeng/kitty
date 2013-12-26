f315062e2911b678675cd37cceb5dc6b6ad86f44
lijiepeng <lijiepeng.tuobi@gmail.com>
2013-12-26 17:49:44 +0800

    commit,

---
+++ src/main/java/com/giantstones/controller/BaseController.java
@@ -0,0 +1,5 @@
+package com.giantstones.controller;
+
+public abstract class BaseController {
+
+}
---
+++ src/main/java/com/giantstones/controller/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author tuobi
+ *
+ */
+package com.giantstones.controller;
